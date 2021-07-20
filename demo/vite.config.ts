6490dfeeed1097e1189f6eccded3dac676edbdf7
a-dev <andytev@gmail.com>
2021-07-20 21:57:02 +0300

    fix a path for the demo on github pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0'
   },
+  // base: '/vue-event-creator/',
+  base: process.env.NODE_ENV === 'production' ? '/vue-event-creator/' : './',
   build: {
     outDir: '../docs'
   }
